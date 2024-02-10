3007a2250adf6d8d30d0bef42a7ca62de8802dc0
stcucufa <githubnyou@romulusetrem.us>
2024-02-10 09:48:08 +0100

    Fix VALUE_NONE

--- value.h
+++ value.h
@@ -14,6 +14,8 @@ typedef union {
 } Value;
 
 #define QNAN 0x7ffc000000000000
+#define NONE (QNAN | 0x0001000000000000)
+#define HAMT_NODE (QNAN | 0x0002000000000000)
 #define OBJECT_MASK 0x0000fffffffffff8
 
 enum {
@@ -31,16 +33,16 @@ enum {
 #define VALUE_NIL (Value){ .as_int = QNAN | tag_nil }
 #define VALUE_FALSE (Value){ .as_int = QNAN | tag_false }
 #define VALUE_TRUE (Value){ .as_int = QNAN | tag_true }
-#define VALUE_NONE (Value){ .as_int = QNAN | tag_string }
-#define VALUE_HAMT_NODE (Value){ .as_int = QNAN | tag_count }
+#define VALUE_NONE (Value){ .as_int = NONE }
+#define VALUE_HAMT_NODE (Value){ .as_int = HAMT_NODE }
 #define VALUE_FROM_STRING(x) (Value){ .as_int = (uintptr_t)(x) | QNAN | tag_string }
 #define VALUE_FROM_NUMBER(x) (Value){ .as_double = (x) }
 #define VALUE_FROM_INT(x) (Value){ .as_double = (double)(x) }
 
 #define VALUE_TAG(v) ((v).as_int & tag_mask)
 
-#define VALUE_IS_NONE(v) ((v).as_int == (QNAN | tag_string))
-#define VALUE_IS_HAMT_NODE(v) ((v).as_int == (QNAN | tag_count))
+#define VALUE_IS_NONE(v) ((v).as_int == NONE)
+#define VALUE_IS_HAMT_NODE(v) ((v).as_int == HAMT_NODE)
 #define VALUE_IS_NIL(v) (VALUE_TAG(v) == tag_nil)
 #define VALUE_IS_BOOLEAN(v) (((v).as_int & 6) == 2)
 #define VALUE_IS_FALSE(v) (VALUE_TAG(v) == tag_false)
