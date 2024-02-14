fe6b969e5379210e4c6ef296e93ac4f7e0ad919a
stcucufa <githubnyou@romulusetrem.us>
2024-02-14 19:45:02 +0100

    Let, environment

--- js/eval.js
+++ js/eval.js
@@ -1,21 +1,21 @@
 // Tokens are either a symbol (e.g. Token.OpenParen for an open parenthesis,
 // Token.End for the end of input) or a value (boolean, number or string).
 const Token = Object.fromEntries([
+    "Boolean", "Number", "String", "Identifier",
     "OpenParen", "CloseParen", "OpenBrace", "CloseBrace",
     "Plus", "Minus", "Star", "StarStar", "Solidus",
     "LE", "LT", "GE", "GT", "Equal", "NE", "Bang",
     "Bar", "Quote", "Unquote",
+    "In", "Let",
     "End"
 ].map(name => ([name, Symbol.for(name)])));
 
 const Keyword = {
-    true: true,
-    false: false,
+    false: [Token.Boolean, false],
+    "let": [Token.Let],
+    "in": [Token.In],
+    true: [Token.Boolean, true],
 };
-
-// Get the type of a token (itself for a symbol, but "boolean", "number" or
-// "string" for values.
-const typeOf = token => typeof token === "symbol" ? Symbol.keyFor(token) : typeof token;
 
 // Generate tokens from an input string.
 function* tokenize(input) {
@@ -43,20 +43,20 @@ function* tokenize(input) {
         switch (c) {
 
             // Simple tokens
-            case "(": yield Token.OpenParen; break;
-            case ")": yield Token.CloseParen; break;
-            case "{": yield Token.OpenBrace; break;
-            case "+": yield Token.Plus; break;
-            case "-": yield Token.Minus; break;
-            case "*": yield isNext("*") ? Token.StarStar : Token.Star; break;
-            case "/": yield Token.Solidus; break;
-            case "<": yield isNext("=") ? Token.LE : Token.LT; break;
-            case ">": yield isNext("=") ? Token.GE : Token.GT; break;
-            case "=": yield Token.Equal; break;
-            case "!": yield isNext("=") ? Token.NE : Token.Bang; break;
-            case "|": yield Token.Bar; break;
-            case "'": yield Token.Quote; break;
-            case "∞": yield Infinity; break;
+            case "(": yield [Token.OpenParen]; break;
+            case ")": yield [Token.CloseParen]; break;
+            case "{": yield [Token.OpenBrace]; break;
+            case "+": yield [Token.Plus]; break;
+            case "-": yield [Token.Minus]; break;
+            case "*": yield isNext("*") ? [Token.StarStar] : [Token.Star]; break;
+            case "/": yield [Token.Solidus]; break;
+            case "<": yield isNext("=") ? [Token.LE] : [Token.LT]; break;
+            case ">": yield isNext("=") ? [Token.GE] : [Token.GT]; break;
+            case "=": yield [Token.Equal]; break;
+            case "!": yield isNext("=") ? [Token.NE] : [Token.Bang]; break;
+            case "|": yield [Token.Bar]; break;
+            case "'": yield [Token.Quote]; break;
+            case "∞": yield [Token.Number, Infinity]; break;
 
             // String: allow interpolation ("x = ${x}") with nesting
             // ("hell${"o" ** 17}, world!") where the string is broken up and
@@ -66,12 +66,12 @@ function* tokenize(input) {
                 if (nesting > 0) {
                     const match = input.match(/^((?:[^"\\\$]|\\.|\$(?!\{))*)("|\$\{)/);
                     if (match) {
-                        yield Token.Star;
+                        yield [Token.Star];
                         input = input.substring(match[0].length);
-                        yield match[1].replace(/\\(.)/g, "$1");
+                        yield [Token.String, match[1].replace(/\\(.)/g, "$1")];
                         if (match[2] === "${") {
-                            yield Token.Star;
-                            yield Token.Quote;
+                            yield [Token.Star];
+                            yield [Token.Quote];
                         } else {
                             nesting -= 1;
                         }
@@ -79,8 +79,8 @@ function* tokenize(input) {
                         throw Error(`Unfinished string near "${input}".`);
                     }
                 } else {
-                    // Just a regular close brace
-                    yield Token.CloseBrace;
+                    // Just a regular closing brace.
+                    yield [Token.CloseBrace];
                 }
                 break;
             }
@@ -88,10 +88,10 @@ function* tokenize(input) {
                 const match = fullInput.match(/^"((?:[^"\\\$]|\\.|\$(?!\{))*)("|\$\{)/);
                 if (match) {
                     input = fullInput.substring(match[0].length);
-                    yield match[1].replace(/\\(.)/g, "$1");
+                    yield [Token.String, match[1].replace(/\\(.)/g, "$1")];
                     if (match[2] === "${") {
-                        yield Token.Star;
-                        yield Token.Quote;
+                        yield [Token.Star];
+                        yield [Token.Quote];
                         nesting += 1;
                     }
                 } else {
@@ -105,15 +105,14 @@ function* tokenize(input) {
                 let match = fullInput.match(/^\d+(\.\d+)?/);
                 if (match) {
                     input = fullInput.substring(match[0].length);
-                    yield parseFloat(match[0]);
+                    yield [Token.Number, parseFloat(match[0])];
                 } else if (match = fullInput.match(/^\w+/)) {
                     input = fullInput.substring(match[0].length);
                     const id = match[0];
                     if (Object.hasOwn(Keyword, id)) {
                         yield Keyword[id];
                     } else {
-                        yield Token.Unquote;
-                        yield id;
+                        yield [Token.Identifier, id];
                     }
                 } else {
                     throw Error(`Unexpected character at "${input}".`);
@@ -121,11 +120,11 @@ function* tokenize(input) {
             }
         }
     }
-    return Token.End;
+    return [Token.End];
 }
 
-// Identity (for value nuds).
-const id = x => x;
+// Value of a literal token (bool, number, string)
+const tokenValue = (_, [__, x]) => x;
 
 // Attempt to apply an unary operator to a value based on its type. Pass a list
 // of (op, type, function) pairs and return a function that applies f to x if
@@ -163,10 +162,10 @@ function repeat(x, n) {
     return r;
 }
 
-// Parse and evaluate an input string. Use a Pratt parser for parsing the
-// expression; the nuds/leds evaluate the input as the parser advances. Return
-// the eventual value.
-export function evaluate(input) {
+// Parse and evaluate an input string in a given environment. Use a Pratt
+// parser for parsing the expression; the nuds/leds evaluate the input as the
+// parser advances. Return the eventual value.
+export function evaluate(input, env = {}) {
 
     // Get an iterator for tokens, keeping track of the current and previous.
     let previousToken, currentToken;
@@ -174,8 +173,8 @@ export function evaluate(input) {
 
     // Advance to the next token, optionally enforcing an expected type.
     function advance(expected) {
-        if (expected && typeOf(expected) !== typeOf(currentToken)) {
-            throw Error(`Expected ${typeOf(expected)}, got ${typeOf(currentToken)}`);
+        if (expected && expected !== currentToken[0]) {
+            throw Error(`Expected ${Symbol.keyFor(expected)}, got ${Symbol.keyFor(currentToken[0])}`);
         }
         previousToken = currentToken;
         currentToken = tokens.next().value;
@@ -184,58 +183,69 @@ export function evaluate(input) {
     // Null denotation for tokens. Literals return their own value, and unary
     // operators.
     const Nud = {
-        Minus: () => unary(["negation", "number", x => -x])(expression(Precedence.unary)),
-        Bang: () => !expression(Precedence.unary),
-        Bar() {
-            const x = expression();
+        Minus: env => unary(["negation", "number", x => -x])(expression(env, Precedence.Unary)),
+        Bang: env => !expression(env, Precedence.Unary),
+        Bar(env) {
+            const x = expression(env);
             advance(Token.Bar);
             return unary(["abs", "number", x => Math.abs(x)], ["length", "string", x => x.length])(x);
         },
-        Quote: () => expression(Precedence.unary).toString(),
-        OpenParen() {
-            const value = expression();
+        Quote: env => expression(env, Precedence.Unary).toString(),
+        OpenParen(env) {
+            const value = expression(env);
             advance(Token.CloseParen);
             return value;
         },
-        number: id,
-        string: id,
-        boolean: id,
+        Let: env => {
+            advance(Token.Identifier);
+            const name = previousToken[1];
+            advance(Token.Equal);
+            const value = expression(env);
+            advance(Token.In);
+            env = Object.create(env);
+            env[name] = value;
+            return expression(env);
+        },
+        Identifier: (env, [_, id]) => env[id],
+        Number: tokenValue,
+        String: tokenValue,
+        Boolean: tokenValue,
     };
 
     // Left denotation of operators. Most are left-associative, which is the
     // default; exponent (StarStar) is right-associative so 
     const Led = {
-        Plus: (x, op) => binary(
+        Plus: (env, x, op) => binary(
             ["addition", "number", "number", (x, y) => x + y]
-        )(x, expression(Precedence[typeOf(op)])),
-        Minus: (x, op) => binary(
+        )(x, expression(env, Precedence[Symbol.keyFor(op[0])])),
+        Minus: (env, x, op) => binary(
             ["subtraction", "number", "number", (x, y) => x - y]
-        )(x, expression(Precedence[typeOf(op)])),
-        Star: (x, op) => binary(
+        )(x, expression(env, Precedence[Symbol.keyFor(op[0])])),
+        Star: (env, x, op) => binary(
             ["multiplication", "number", "number", (x, y) => x * y],
             ["concatenation", "string", "string", (x, y) => x + y]
-        )(x, expression(Precedence[typeOf(op)])),
-        Solidus: (x, op) => binary(
+        )(x, expression(env, Precedence[Symbol.keyFor(op[0])])),
+        Solidus: (env, x, op) => binary(
             ["division", "number", "number", (x, y) => x / y]
-        )(x, expression(Precedence[typeOf(op)])),
-        StarStar: (x, op) => binary(
+        )(x, expression(env, Precedence[Symbol.keyFor(op[0])])),
+        StarStar: (env, x, op) => binary(
             ["exponentiation", "number", "number", (x, y) => x ** y],
             ["repetition", "string", "number", (x, y) => repeat(x, y)]
-        )(x, expression(Precedence[typeOf(op) - 1])),
-        LT: (x, op) => binary(
+        )(x, expression(env, Precedence[Symbol.keyFor(op[0])] - 1)),
+        LT: (env, x, op) => binary(
             ["comparison (less than)", "number", "number", (x, y) => x < y]
-        )(x, expression(Precedence[typeOf(op)])),
-        LE: (x, op) => binary(
+        )(x, expression(env, Precedence[Symbol.keyFor(op[0])])),
+        LE: (env, x, op) => binary(
             ["comparison (less or equal)", "number", "number", (x, y) => x <= y]
-        )(x, expression(Precedence[typeOf(op)])),
-        GT: (x, op) => binary(
+        )(x, expression(env, Precedence[Symbol.keyFor(op[0])])),
+        GT: (env, x, op) => binary(
             ["comparison (greater than)", "number", "number", (x, y) => x > y]
-        )(x, expression(Precedence[typeOf(op)])),
-        GE: (x, op) => binary(
+        )(x, expression(env, Precedence[Symbol.keyFor(op[0])])),
+        GE: (env, x, op) => binary(
             ["comparison (greater or equal)", "number", "number", (x, y) => x >= y]
-        )(x, expression(Precedence[typeOf(op)])),
-        Equal: (x, op) => x === expression(Precedence[typeOf(op)]),
-        NE: (x, op) => x !== expression(Precedence[typeOf(op)]),
+        )(x, expression(env, Precedence[Symbol.keyFor(op[0])])),
+        Equal: (env, x, op) => x === expression(env, Precedence[Symbol.keyFor(op[0])]),
+        NE: (env, x, op) => x !== expression(env, Precedence[Symbol.keyFor(op[0])]),
     };
 
     // Precedence levels of operators: interpolation is used for string
@@ -249,27 +259,27 @@ export function evaluate(input) {
         Plus: 4, Minus: 4,
         Star: 5, Solidus: 5,
         StarStar: 6,
-        unary: 7,
+        Unary: 7,
     };
 
     // The core of the parser loop. Apply the null denotation to the current
     // token and advance; then while the current token has a higher precedence
     // than the target precedence, advance and apply the left denotation of the
     // previous token.
-    function expression(precedence = 0) {
-        const nud = Nud[typeOf(currentToken)];
+    function expression(env, precedence = 0) {
+        const nud = Nud[Symbol.keyFor(currentToken[0])];
         if (!nud) {
-            throw Error("Expected nud");
+            throw Error(`Expected nud for ${Symbol.keyFor(currentToken[0])}.`);
         }
         advance();
-        let x = nud(previousToken);
-        while (Precedence[typeOf(currentToken)] > precedence) {
+        let x = nud(env, previousToken);
+        while (Precedence[Symbol.keyFor(currentToken[0])] > precedence) {
             advance();
-            const led = Led[typeOf(previousToken)];
+            const led = Led[Symbol.keyFor(previousToken[0])];
             if (led) {
-                x = led(x, previousToken);
+                x = led(env, x, previousToken);
             } else {
-                throw Error("Expected led");
+                throw Error(`Expected led for ${Symbol.keyFor(previousToken[0])}.`);
             }
         }
         return x;
@@ -278,7 +288,7 @@ export function evaluate(input) {
     // Move to the first token and parse an expression; then check that we
     // reached the end of input.
     advance();
-    const result = expression();
+    const result = expression(env);
     advance(Token.End);
     return result;
 }
--- js/eval.test.js
+++ js/eval.test.js
@@ -1,24 +1,32 @@
 import { describe, expect, test } from "bun:test";
 import { evaluate } from "./eval.js";
 
-const tests = {
-    "-1 + 2 * 3 - 4": -1 + 2 * 3 - 4,
-    "(1 + 2) * -3 - 4": (1 + 2) * -3 - 4,
-    "2 ** 3 ** 4": 2 ** 3 ** 4,
-    "\"\\\"foo\\\"\" * \"bar\"": "\"foo\"" + "bar",
-    "\"length of foo = ${|\"foo\"|}, false = ${!true}\"": "length of foo = 3, false = false",
-    "!(1 + 2 < 3 + 4)": !(1 + 2 < 3 + 4),
-    "|-|\"foo\" * \"bar\"||": 6,
-    "|'123|": 3,
-    "\"hell\" * \"o\" ** 17": "hellooooooooooooooooo",
-    "!true": false,
-    "-∞": -Infinity,
-};
-
-describe("Evaluate expressions", () => {
+const testAll = tests => {
     for (const [input, output] of Object.entries(tests)) {
         test(input, () => { expect(evaluate(input)).toEqual(output); });
     }
+}
+
+describe("Evaluate expressions", () => {
+    testAll({
+        "-1 + 2 * 3 - 4": -1 + 2 * 3 - 4,
+        "(1 + 2) * -3 - 4": (1 + 2) * -3 - 4,
+        "2 ** 3 ** 4": 2 ** 3 ** 4,
+        "\"\\\"foo\\\"\" * \"bar\"": "\"foo\"" + "bar",
+        "\"length of foo = ${|\"foo\"|}, false = ${!true}\"": "length of foo = 3, false = false",
+        "!(1 + 2 < 3 + 4)": !(1 + 2 < 3 + 4),
+        "|-|\"foo\" * \"bar\"||": 6,
+        "|'123|": 3,
+        "\"hell\" * \"o\" ** 17": "hellooooooooooooooooo",
+        "!true": false,
+        "-∞": -Infinity,
+    });
+});
+
+describe("Variables and environment", () => {
+    testAll({
+        "let pi = 3.141592653589793 in 2 * pi": 2 * Math.PI
+    });
 });
 
 test("Runtime error", () => {
