e662079a3ca9fdef6db18d537067ca8320bfbadc
stcucufa <githubnyou@romulusetrem.us>
2024-02-15 17:53:29 +0100

    Test Fibonacci

--- hamt-test.c
+++ hamt-test.c
@@ -1,3 +1,4 @@
+#include <inttypes.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -12,10 +13,40 @@ void test_numbers(void) {
     for (size_t i = 0; i < 1131152; ++i) {
         hamt_set(&hamt, VALUE_FROM_NUMBER(i), VALUE_FROM_NUMBER(0));
     }
-    // hamt_free(&hamt);
+    hamt_free(&hamt);
+}
+
+double fibonacci(double i) {
+    return i < 2 ? i : fibonacci(i - 1) + fibonacci(i - 2);
+}
+
+double fibonacci_memoized(double i, HAMT* hamt) {
+    if (i < 2) {
+        return i;
+    }
+
+    Value key = VALUE_FROM_NUMBER(i);
+    Value v = hamt_get(hamt, key);
+    if (!VALUE_IS_NONE(v)) {
+        return v.as_double;
+    }
+    double f1 = fibonacci_memoized(i - 1, hamt);
+    double f2 = fibonacci_memoized(i - 2, hamt);
+    double f = f1 + f2;
+    hamt_set(hamt, key, VALUE_FROM_NUMBER(f));
+    return f;
+}
+
+uint64_t test_fibonacci(double i) {
+    HAMT hamt;
+    hamt_init(&hamt);
+
+    double f = fibonacci_memoized(i, &hamt);
+    hamt_free(&hamt);
+    return f;
 }
 
 int main(int argc, char* argv[argc + 1]) {
-    test_numbers();
+    printf("%" PRId64 "\n", test_fibonacci(10000));
     return EXIT_SUCCESS;
 }
